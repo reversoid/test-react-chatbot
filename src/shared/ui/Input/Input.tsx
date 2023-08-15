5b940d862ff5608feae3f63cb9f29d4dcfb2f449
George Kobunov <ancient.gekob@gmail.com>
2023-08-16 00:26:48 +0300

    Save send image

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,15 +1,19 @@
-import { InputHTMLAttributes, forwardRef } from "react";
+import { InputHTMLAttributes, ReactNode, forwardRef } from "react";
 import styles from "./styles.module.scss";
 import { classNames } from "../../utils/classnames";
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  contentRight?: ReactNode;
+}
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  return (
-    <input
-      {...props}
-      className={classNames(styles.input, props.className)}
-      ref={ref}
-    />
-  );
-});
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ contentRight, ...props }, ref) => {
+    return (
+      <input
+        {...props}
+        className={classNames(styles.input, props.className)}
+        ref={ref}
+      />
+    );
+  }
+);
